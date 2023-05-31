c8fb47887c00ce81129c6b3fc60bf1cba5ffa06c
veenard <veenadhoke880@gmail.com>
2023-05-31 22:12:07 +0530

    AdditionOperation

---
+++ AdditionOperation/src/com/sathya/gitproject/Addition.java
@@ -0,0 +1,14 @@
+package com.sathya.gitproject;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int num1=10;
+		int num2=20;
+		int result;
+		result=num1+num2;
+		System.out.println("Addition of two number is "+result);
+
+	}
+
+}
